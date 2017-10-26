a0cd0d222755c96d9683be48036f3108fe521f42
Bui Minh Quang <bmq928@gmail.com>
2017-10-26 13:37:52 +0700

    change_procfile

--- app.js
+++ app.js
@@ -57,14 +57,14 @@ fs.writeFile('public/client_app.min.js', uglifiedCode, function (err) {
 });
 
 // uncomment after placing your favicon in /public
-
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 
 //init passport
 app.use(passport.initialize());
