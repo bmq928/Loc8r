ae0f6ffcec47ab4bda10a8ab2733ae1b551d7cf9
Bui Minh Quang <bmq928@gmail.com>
2017-11-08 21:45:43 +0700

    fix-server.js

--- bin/server.js
+++ bin/server.js
@@ -20,9 +20,7 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+var server = app.listen(process.env.PORT || 3000);
 /**
  * Listen on provided port, on all network interfaces.
  */
